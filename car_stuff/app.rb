284e29d9f944c299f1aed884b36cd5d4cd6292ac
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-03-29 10:12:23 -0400

    Add constructor with instant variables

--- car_stuff/app.rb
+++ car_stuff/app.rb
@@ -2,5 +2,5 @@
 
 require_relative "lib/car.rb"
 
-my_car = Car.new
+my_car = Car.new("black", "Mazga", "Mazga3")
 my_car.rev
--- car_stuff/lib/car.rb
+++ car_stuff/lib/car.rb
@@ -1,7 +1,18 @@
 # lib/car.rb
 
 class Car
+  def initialize(color, make, model)
+    @color = color
+    @make = make
+    @model = model
+    @miles = 0
+  end
+
   def rev
     puts "Broom"
+  end
+
+  def set_price(price)
+    @price = price
   end
 end
